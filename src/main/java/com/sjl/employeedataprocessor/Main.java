b592e8645a7acd3ef58c41e9afe9630e728b00b4
Shenjie Lin <linshenjie321@gmail.com>
2021-05-11 12:20:31 -0400

    added ability to process supply option data

--- src/main/java/com/sjl/employeedataprocessor/Main.java
+++ src/main/java/com/sjl/employeedataprocessor/Main.java
@@ -3,8 +3,8 @@ package com.sjl.employeedataprocessor;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -14,10 +14,16 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Main {
 	
+	static String folderLocation = "C:\\Users\\Cometstrike\\jee-2021-03-workspace\\Employee-Data-Processor\\test-data";
+	static String outputFileName = "(MODIFIED) GOOGLE RESPONSES - ESL_LINC Summer Bidding Application 2021 - Processed.xlsx";
+	static String outputFileLocation = folderLocation + "\\" + outputFileName;
+	
 	public static void main(String[] args) {
 		String folderLocation = "C:\\Users\\Cometstrike\\jee-2021-03-workspace\\Employee-Data-Processor\\test-data";
 		String fileName = "(MODIFIED) GOOGLE RESPONSES - ESL_LINC Summer Bidding Application 2021 - Copy.xlsx";
 		String fileLocation = folderLocation + "\\" + fileName;
+		
+		
 		
 		readExcelSheet(fileLocation, 0);
 		
@@ -40,21 +46,29 @@ public class Main {
 					i++;
 					continue;
 				}
-				Map<Integer, String> columnHeader = summerBiddingApplication.getColumnHeader();
-				for(int j = 0; j < columnHeader.size(); j++) {
-					Cell cell = row.getCell(j);
-					//we assume the cell column header is string type
-					readCellValue(cell, columnHeader.get(j).toString());
-				}
+				supplyOptionProcessor(row);
 				i++;
 			}
-			
+
+			FileOutputStream outputStream = new FileOutputStream(outputFileLocation);
+			workbook.write(outputStream);
 			workbook.close();
 			file.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private static void supplyOptionProcessor(Row row) {
+		Cell supplyOption = row.getCell(0);
+		if("I am ONLY interested in supply work for the summer.".equalsIgnoreCase(supplyOption.getStringCellValue())) {
+			row.getCell(1).setCellValue("X");
+		} else if("I would like to supply for the summer if I do not get an assignment.".equalsIgnoreCase(supplyOption.getStringCellValue())) {
+			row.getCell(2).setCellValue("X");
+		} else if("If I obtain a summer assignment, I also wish to supply for the summer outside of my assignment hours.".equalsIgnoreCase(supplyOption.getStringCellValue())) {
+			row.getCell(3).setCellValue("X");
 		}
 	}
 	
